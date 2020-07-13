3626cd30e3094a9156cb069cb3bfa7c9f792a215
Barış Çetintürk <baris.cetinturk.ytu@gmail.com>
2020-07-13 13:32:08 +0300

    Initial commit

---
+++ src/main/java/com/example/kafein_staj/KafeinStajApplication.java
@@ -0,0 +1,13 @@
+package com.example.kafein_staj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafeinStajApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KafeinStajApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/kafein_staj/KafeinStajApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.kafein_staj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafeinStajApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
