2aca6afb983b0cf9ca0108d5d99ae4bbfedf3cec
Barış Çetintürk <baris.cetinturk.ytu@gmail.com>
2020-07-24 11:48:08 +0300

    Made http status NOT_FOUND

--- src/main/java/com/example/kafein_staj/exception/EntityNotFoundException.java
+++ src/main/java/com/example/kafein_staj/exception/EntityNotFoundException.java
@@ -3,7 +3,7 @@ package com.example.kafein_staj.exception;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
-@ResponseStatus(value = HttpStatus.BAD_REQUEST, reason = "Could not find entity with id.")
+@ResponseStatus(value = HttpStatus.NOT_FOUND, reason = "Could not find entity with id.")
 public class EntityNotFoundException extends Exception{
     public EntityNotFoundException(String message)
     {
